47ddc97924c7b3e3bc1d84039ddb280e3ed6daf0
Thavi <thavit97@gmail.com>
2020-08-17 14:54:06 +0100

    Complete

--- contact.php
+++ contact.php
@@ -11,4 +11,11 @@ $recipient = "test@localhost.com";
 $subject = "Example Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Your contact form has been sent!";
+?>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <title>Thank You!</title>
+  <p>Your contact form has been sent! <a href="/TreeExercisePHP">Click here</a> to return!</p>
+</head>
+</html>
